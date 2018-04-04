e9a35c508c5e579e92577dcc45e48f9d7b632a33
Sierra Sarver <SSARVER@Rollins.edu>
2018-04-04 13:00:42 -0400

    Adding music page

---
+++ django_branch/capstone_django_branch/music/static/music/js/music.js
@@ -0,0 +1,14 @@
+// Music JavaScript
+
+$('.play').click(function() {
+  var $this = $(this);
+  var id = $this.attr('id').replace(/btn/, '');
+  $this.toggleClass('active');
+  if ($this.hasClass('active')) {
+      $this.text('pause');
+      $('audio[id^="sound"]')[id-1].play();
+  } else {
+      $this.text('play');
+      $('audio[id^="sound"]')[id-1].pause();
+  }
+});
