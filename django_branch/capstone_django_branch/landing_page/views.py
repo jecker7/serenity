854ac8bbbc642a6e90a5973e81234fc950f26e79
john <jhecker123@gmail.com>
2018-04-06 18:00:37 -0400

    changed landing_page to a modal style log-in

--- django_branch/capstone_django_branch/landing_page/views.py
+++ django_branch/capstone_django_branch/landing_page/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from accounts.custom_forms import RegisterUserForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
 
 def landing_page(request):
-    return render(request, 'landing_page/landing_page.html')
+    #detect get or post
+    if request.method == 'POST':
+        form1 = RegisterUserForm(request.POST)
+        form2 = AuthenticationForm(data = request.POST)
+        (request.POST)
+        #check if form is valid
+        if form1.is_valid():
+            user = form.save()
+            #log the user in after
+            #redirect to /home
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'landing_page/landing_page.html/', {'form':form})
+
+        if form2.is_valid():
+            #log in the user
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form1 = AuthenticationForm()
+
+        form2 = RegisterUserForm()
+    return render(request, 'landing_page/landing_page.html/', {'form1': form1, 'form2' : form2})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('landing_page')
